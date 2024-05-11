c6318c02c9dbc549204f31b31e38d959fc03597e
Itay <151715344+Abnomality@users.noreply.github.com>
2024-05-11 13:13:40 -0700

    Testing default

--- a3/script.js
+++ a3/script.js
@@ -27,16 +27,28 @@ function upLeft(rHeight, colorEven, colorOdd, symbol) {
 
 
 
-function upRight(rHeight, colorEven, colorOdd, symbol) {
-  var rLine = "";
-  for (var i = 1; i <= rHeight; i++) {
-    rLine += "<p>";
-    for (var j = 1; j <= i; j++) {
-      rLine += "<span style='color:" + (j % 2 ? colorEven : colorOdd) + ";'>" + symbol + "</span>";
-    }
-    rLine += "</p>";
-  }
-  document.getElementById("upRight").innerHTML = rLine;
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=0;i<pHeight;i++){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<=i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upRight").innerHTML = rLine;
 }
 
 function downLeft(rHeight, colorEven, colorOdd, symbol) {
@@ -51,14 +63,28 @@ function downLeft(rHeight, colorEven, colorOdd, symbol) {
   document.getElementById("downLeft").innerHTML = rLine;
 }
 
-function downRight(rHeight, colorEven, colorOdd, symbol) {
-  var rLine = "";
-  for (var i = rHeight; i >= 1; i--) {
-    rLine += "<p>";
-    for (var j = 1; j <= rHeight - i; j++) {
-      rLine += "<span style='color:" + (j % 2 ? colorEven : colorOdd) + ";'>" + symbol + "</span>";
-    }
-    rLine += "</p>";
-  }
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<i;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downRight").innerHTML = rLine;
+}
   document.getElementById("downRight").innerHTML = rLine;
 }
