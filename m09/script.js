e09a5ef1f3d78193835671c659132089c247a9b8
Itay <151715344+Abnomality@users.noreply.github.com>
2024-06-01 11:49:39 -0700

    Create script.js

---
+++ m09/script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var mybutton = document.querySelector("#clearButton");
+    var colorPicker = document.querySelector("#colorPicker");
+    var sizePicker = document.querySelector("#sizePicker");
+
+    mybutton.addEventListener("click", function(event) {
+        var elements = document.getElementsByClassName("dot");
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+
+        // Stop the propagation of events
+        event.stopPropagation();
+    });
+
+    addEventListener("click", function(event) {
+        // Do not draw a dot if the clear button is clicked
+        if (event.target === mybutton) return;
+
+        var dot = document.createElement("div");
+        dot.className = "dot";
+        dot.style.left = (event.pageX - sizePicker.value / 2) + "px";
+        dot.style.top = (event.pageY - sizePicker.value / 2) + "px";
+        dot.style.height = dot.style.width = sizePicker.value + "px";
+        dot.style.background = colorPicker.value;
+        document.body.appendChild(dot);
+    });
+});
