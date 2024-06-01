d19891b3b2c560638e48afdc1d214b75b27ef4a9
Itay <151715344+Abnomality@users.noreply.github.com>
2024-06-01 12:05:53 -0700

    Create script.js

---
+++ a5/script.js
@@ -0,0 +1,42 @@
+const images = [
+    { src: 'images/image1.jpg', description: 'Description of Image 1' },
+    { src: 'images/image2.jpg', description: 'Description of Image 2' },
+    { src: 'images/image3.jpg', description: 'Description of Image 3' },
+    { src: 'images/image4.jpg', description: 'Description of Image 4' },
+    { src: 'images/image5.jpg', description: 'Description of Image 5' }
+];
+
+let currentIndex = 0;
+let interval;
+
+const imageElement = document.getElementById('slideshow-image');
+const descriptionElement = document.getElementById('description');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+function showImage(index) {
+    imageElement.src = images[index].src;
+    descriptionElement.textContent = images[index].description;
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+    resetInterval();
+}
+
+function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+    resetInterval();
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(nextImage, 3000);
+}
+
+prevBtn.addEventListener('click', prevImage);
+nextBtn.addEventListener('click', nextImage);
+
+interval = setInterval(nextImage, 3000);
