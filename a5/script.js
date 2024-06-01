616bbacc6320be982c27c662506af2328948cd32
Itay <151715344+Abnomality@users.noreply.github.com>
2024-06-01 12:30:02 -0700

    Update script.js

--- a5/script.js
+++ a5/script.js
@@ -1,9 +1,9 @@
 const images = [
-    { src: 'images/image1.jpg', description: 'Description of Image 1' },
-    { src: 'images/image2.jpg', description: 'Description of Image 2' },
-    { src: 'images/image3.jpg', description: 'Description of Image 3' },
-    { src: 'images/image4.jpg', description: 'Description of Image 4' },
-    { src: 'images/image5.jpg', description: 'Description of Image 5' }
+    { src: 'images/image1.png', description: 'This is my first artifact assignment in dev 109. We learned how to bold, italicize, and space text in HTML.' },
+    { src: 'images/image2.png', description: 'This is a static chessboard we made. It helped show us how to center bojects and images and how IDs work.' },
+    { src: 'images/image3.png', description: 'This is a rhombus generator. It was a great exercise on how to implement Javascript with HTML to make a shape generator.' },
+    { src: 'images/image4.png', description: 'This is a project where we had to create a form for a usar to complete. You had to make sure all the fields worked, and to throw the usar an error message if something was wrong.' },
+    { src: 'images/image5.png', description: 'This is a module assignment where we had to track the curser and whenever the curser clicks it draws a dot.' }
 ];
 
 let currentIndex = 0;
@@ -33,7 +33,7 @@ function prevImage() {
 
 function resetInterval() {
     clearInterval(interval);
-    interval = setInterval(nextImage, 10000);  // 10000ms = 10s
+    interval = setInterval(nextImage, 10000);
 }
 
 prevBtn.addEventListener('click', prevImage);
